ad772ce0133498f0db1f1a56929886309615d0e5
roblee357 <roblee357@hotmail.com>
2021-02-04 16:08:48 -0500

    creating subscriber class

--- Assignment1/subscriber.py
+++ Assignment1/subscriber.py
@@ -1,27 +1,6 @@
-
-
 import sys, zmq, json
 
 class Subscriber():
-
-    # def setup_socket(self,topic,sub_id):
-    #     # Socket to talk to server
-    #     with open('config.json','r') as fin:
-    #         config = json.load(fin)
-
-    #     context = zmq.Context()
-    #     socket = context.socket(zmq.SUB)
-
-    #     print("Socket setup...")
-
-    #     bconnectionstring = str.encode('tcp://' + config['ip'] + ":" + config['port'])
-    #     socket.connect (bconnectionstring)
-
-    #     # Subscribe to zipcode, default is NYC, 10001
-    #     topicfilter = str.encode(args.topic)
-    #     socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
-    #     return socket
-
     def __init__(self, topic,sub_id):
         self.topic = topic
         self.sub_id = sub_id
@@ -32,31 +11,24 @@ class Subscriber():
         #  Socket to talk to server
         print("Connecting to broker...")
         self.socket = context.socket(zmq.SUB)
-        self.con_str = "tcp://" + config['ip'] + ":" + config['port']
+        self.con_str = "tcp://" + config['ip'] + ":" + config['sub_port']
         print(self.con_str)
         self.socket.connect(self.con_str)
         topicfilter = str.encode(self.topic)
         self.socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
-        ## Registering ## Commented out as each message is headered with pud_id and topic.
-        # message = 'PUB_:_' + str(pub_id) + '_:_' + topic + '_:_registering'
-        # register_reply = self.send(message)
-        # print(register_reply)
 
     def run(self):
         print('sub_id',self.sub_id,'subscribed to:' ,self.topic,'on', self.con_str)
         while True:
             string = self.socket.recv()
-            print(string)        
+            print(string)
 
 # main
 def main():
     sub1 = Subscriber('topic1',1)
-    sub1.run()
+    string = sub1.run()
 
 
 #----------------------------------------------
 if __name__ == '__main__':
     main ()
-
-
-
--- Assignment1/subscriber_no_class.py
+++ Assignment1/subscriber_no_class.py
@@ -1,20 +1,16 @@
 import sys, argparse
 import zmq
 
-
 def parseCmdLineArgs ():
     # parse the command line
     parser = argparse.ArgumentParser ()
-
     # optional arguments
     parser.add_argument ("-bip", "--brokerip",  default='localhost', help="IP addr of broker")
     parser.add_argument ("-bp", "--brokerport",   default='5556', help="IP addr of broker")
     parser.add_argument ("-m", "--message",   default='word', help="message")
     parser.add_argument ("-id", "--id", type=int,   default=0, help="ID (int)")
-
     # # add positional arguments in that order
     parser.add_argument ("topic", help="topic name")
-
     # parse the args
     args = parser.parse_args ()
     return args
@@ -23,12 +19,8 @@ def setup_socket(args):
     # Socket to talk to server
     context = zmq.Context()
     socket = context.socket(zmq.SUB)
-
-    print("Socket setup...")
-
     bconnectionstring = str.encode('tcp://' +  args.brokerip  + ':' + args.brokerport)
     socket.connect (bconnectionstring)
-
     # Subscribe to zipcode, default is NYC, 10001
     topicfilter = str.encode(args.topic)
     socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
@@ -40,16 +32,13 @@ def main():
     args = parseCmdLineArgs ()
     # setup socket with commandline arguments
     socket = setup_socket(args)
-
     total_value = 0
     while True:
         string = socket.recv()
         print(string)
 
 
+
 #----------------------------------------------
 if __name__ == '__main__':
     main ()
-
-
-
