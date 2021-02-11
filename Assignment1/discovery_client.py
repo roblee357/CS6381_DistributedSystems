0d37cf23f17ad8ccca46de175906f4b413f1f41c
roblee357 <roblee357@hotmail.com>
2021-02-11 13:39:40 -0500

    discovery client and server

---
+++ Assignment1/discovery_client.py
@@ -0,0 +1,66 @@
+import zmq,  json, sys
+import argparse, time
+
+def parseCmdLineArgs ():
+    # parse the command line
+    parser = argparse.ArgumentParser ()
+    # add optional arguments
+    parser.add_argument ("-i", "--ip", default='localhost',help="IP address of broker/proxy")
+    parser.add_argument ("-s", "--sip", default='localhost',help="IP address of discovery server")
+    # add positional arguments in that order
+    parser.add_argument ("topic", help="Topic")
+    parser.add_argument ("id", help="ID")
+    # parse the args
+    args = parser.parse_args ()
+    return args
+
+class Dclient():
+
+    def __init__(self, ispub, topic,pub_id,ip,sip):
+        self.ispub = ispub
+        self.topic = topic
+        self.pub_id = pub_id
+        self.ip = ip
+        self.sip = sip
+        self.context = zmq.Context()
+        with open('config.json','r') as fin:
+            config = json.load(fin)
+        self.port = config['disc_port']
+        con_str = "tcp://" + self.ip + ":" + self.port
+        print(con_str)
+        self.socket = self.context.socket(zmq.REQ)
+        self.socket.connect(con_str)
+        time.sleep(.5)
+
+    def broadcast(self):
+        message =  self.ispub + ',' +self.topic + ',' + str(self.pub_id) + ',' + str(self.sip)
+        self.socket.send_string(message)
+        reply = self.socket.recv()
+        return reply
+
+def main ():
+    """ Main program for publisher. This will be the publishing application """
+    args = parseCmdLineArgs ()
+    dclient = Dclient('PUB',args.topic,args.id,args.ip,args.sip)
+    for i in range(1):
+        discovery_server_response = dclient.broadcast()
+    print('discovery_server_response',discovery_server_response)
+
+    dclient2 = Dclient('PUB','topic2','2','localhost','10.0.0.2')
+    for i in range(1):
+        discovery_server_response = dclient2.broadcast()
+    print('discovery_server_response',discovery_server_response)
+
+    dclient3 = Dclient('PUB','topic2','3','localhost','10.0.0.3')
+    for i in range(1):
+        discovery_server_response = dclient3.broadcast()
+    print('discovery_server_response',discovery_server_response)
+
+    dclient4 = Dclient('SUB','topic2','4','localhost','10.0.0.3')
+    for i in range(1):
+        discovery_server_response = dclient4.broadcast()
+    print('discovery_server_response',discovery_server_response)
+
+#----------------------------------------------
+if __name__ == '__main__':
+    main ()
---
+++ Assignment1/discovery_server.py
@@ -0,0 +1,62 @@
+import sys
+import time
+import random
+import json
+import zmq
+
+
+class Dserve:
+
+    def __init__(self):
+        with open('config.json','r') as fin:
+            config = json.load(fin)
+        self.port = config['disc_port']
+        # Request - Reply socket
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.REP)
+        self.socket.bind("tcp://*:" + self.port)
+        # The publishers do not need to know about the subscribers, but 
+        # the subscribers need to know the addresses of the publishers
+        # Dictionary for holding subcriber and publisher information
+        self.registry = {}
+        self.registry['PUB'] = {}
+        self.registry['SUB'] = {}
+
+       
+    def run(self):
+        with open('config.json','r') as fin:
+            config = json.load(fin)
+        while True:
+            #  Wait for next request from client
+            # print('started loop')
+            self.message = self.socket.recv()
+            self.decoded_message = self.message.decode("utf-8")
+            # print('self.decoded_message',self.decoded_message)
+            self.deliminated_message = self.decoded_message.split(',')
+            self.app_type = self.deliminated_message[0]
+            self.topic = self.deliminated_message[1]
+            self.ID = self.deliminated_message[2]
+            self.ip = self.deliminated_message[3]
+            # print(self.app_type,self.topic,self.ID,self.message)
+            try:
+                self.registry[self.app_type][self.topic][self.ID] = self.ip
+            except Exception as e:
+                self.registry[self.app_type][self.topic] = { self.ID  :  self.ip }
+                print(e)
+            ALT = 'SUB' if self.app_type == 'PUB' else 'PUB'
+            print(self.registry)
+            try:
+                reply = json.dumps(self.registry[ALT][self.topic])
+            except:
+                reply = 'None found'
+            self.bmessage = str.encode(ALT + 's discovered: ' + reply)
+            self.socket.send(self.bmessage)
+
+def main():
+    print('Instantiating the discovery server')
+    dserver = Dserve()
+    dserver.run()
+
+#----------------------------------------------
+if __name__ == '__main__':
+    main ()
