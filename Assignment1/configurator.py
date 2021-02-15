2d5c5dbdc2407c3a9d6cadf746be2269c02ba1c4
Rob <roblee357@hotmail.com>
2021-02-15 09:56:45 -0500

    discovery_server_OR_broker.py

--- Assignment1/configurator.py
+++ Assignment1/configurator.py
@@ -3,15 +3,18 @@ import json
 def change(key,value):
     with open('config.json','r') as fin:
         config = json.load(fin)
+    oldVal = config[key]
     if isinstance(value, str):
         if 'toggle' in value:
+            print('configurator toggling value')
             config[key] = not config[key]
         else:
             config[key] = value
     else:
-        config[key] = not config[key]
+        config[key] = value
     with open('config.json','w') as fout:
         fout.write(json.dumps(config))
+    print('Configurator changed',key,'from',oldVal,'to',config[key])
     return config
 
 def main():
