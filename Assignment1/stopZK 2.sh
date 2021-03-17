766c5639e66ffd6c46ab3267000bf59e14ee750b
Jessica Phelan <jessiephelan22@gmail.com>
2021-03-16 21:29:45 -0400

    zk.py changes- leader_timeout

---
+++ Assignment1/stopZK 2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/rob/Desktop/apache-zookeeper-3.6.2-bin/bin/zkServer.sh stop
--- Assignment1/zk 2.py
+++ Assignment1/zk 2.py
@@ -54,7 +54,7 @@ class ZK:
                 print(lead_broker_name, broker, broker_data,mtime)
                 if broker == lead_broker_name:
                     leader_age = curTime - mtime
-                    if leader_age > self.config["leader_timeout"]
+                    if leader_age > 300:
                         print('Leader is old. Let\'s get rid of they.', leader_age)
                         self.claim_lead()
                     else:
@@ -64,7 +64,7 @@ class ZK:
     def continuousLeaderCheck(self):
         while True:
             self.checkIfLeader()
-            time.sleep(self.config["leader_check_frequency"])
+            time.sleep(.5)
 
     def start_leader_checks(self):
         t = Thread(target=self.continuousLeaderCheck)
