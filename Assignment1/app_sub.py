961568595e7e5f2d0117f6e650a1c065cbb9e9d8
roblee357 <roblee357@hotmail.com>
2021-04-07 08:42:24 -0400

    removed old files

--- Assignment1/app_sub.py
+++
@@ -1,8 +0,0 @@
-from subscriber import *
-
-# publisher.main
-
-sub1 = Subscriber('topic2',3)
-while True:
-    reply = sub1.run()
-    print(reply)
--- Assignment1/stopZK 2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/home/rob/Desktop/apache-zookeeper-3.6.2-bin/bin/zkServer.sh stop
