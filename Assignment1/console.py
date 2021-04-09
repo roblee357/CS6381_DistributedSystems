f2ebcbb2b669c4436205e77b72d978e0047511dc
roblee357 <roblee357@hotmail.com>
2021-04-09 12:36:14 -0400

    history 2

--- Assignment1/console.py
+++ Assignment1/console.py
@@ -1,16 +1,11 @@
-import sys
 from kazoo.client import KazooClient
-import kazoo.recipe.watchers
-import json 
-import time
-
+import time, json
 
 class ZK_Viewer:
-    def __init__(self):
-        with open('config.json','r') as fin:
-            config = json.load(fin)
-        self.zk = KazooClient(hosts= config['zkip'] + ':2181')
+    def __init__(self,host):
 
+        self.zk = KazooClient(hosts= host)
+        self.exclude = ['zookeeper']
         self.zk.start()
         self.data_dict = {}
         self.line = ''
@@ -19,25 +14,24 @@ class ZK_Viewer:
         children = self.zk.get_children(root)
         i = 0
         for child in children:
-            if (level == 0) and (i == 0):
-                self.line = ''
-            try:
-                data, znode_stats = self.zk.get(root + "/" + child)
-                data = data.decode('utf-8')
-
-            
-            
-                indent = ' '*4*level
-                # print(indent,child,data)
-                self.line += indent + child + ' ' + data + '\n'
-                self.data_dict[root + "/" + child] = {"data":data,"children":self.list_data(root + "/" + child, level = level + 1)}
-                i += 1
-            except:
-                data = 'None'
-
+            if not child in self.exclude:
+                if (level == 0) and (i == 0):
+                    self.line = ''
+                try:
+                    data, znode_stats = self.zk.get(root + "/" + child)
+                    data = data.decode('utf-8')
+                    indent = ' '*4*level
+                    # print(indent,child,data)
+                    self.line += indent + child + ' ' + data + '\n'
+                    self.data_dict[root + "/" + child] = {"data":data,"children":self.list_data(root + "/" + child, level = level + 1)}
+                    i += 1
+                except:
+                    data = 'None'
 
 def main():
-    zkv = ZK_Viewer()
+    with open('config.json','r') as fin:
+        config = json.load(fin)
+    zkv = ZK_Viewer(config['zkip'] + ':2181')
     while True:
         zkv.list_data('')
         # print(zkv.data_dict)
@@ -45,12 +39,5 @@ def main():
             print('')
         print(zkv.line)
 
-        # time.sleep(.1)
-
 if __name__ == '__main__':
     main()
-
-
-    # sys.stdout.write("\rDoing thing %s" % ','.join(children[:3]))
-    # sys.stdout.flush()
-    # time.sleep(.1)
